50aa48526cd3ed1726ec944d49762ba6a9fcc02f
Joloper <62996161+Joloper@users.noreply.github.com>
2021-05-24 10:27:46 +0400

    Create Grad.h

---
+++ Grad/Grad.h
@@ -0,0 +1,41 @@
+#pragma once
+
+using namespace std;
+
+/* JOLOPER INC. All rights reserved
+   Grad is a C/C++ Library
+   Make sure everything is correct
+   If you mess up something, the Grad Code might give errors
+   Use Grad correctly
+   Made By: AxilJ
+
+   Grad TM
+*/
+
+// INCLUDES
+#include "Grad.h"
+#include <iostream>
+
+/*
+   Grad
+   Glad
+   GLFW
+   OpenGL
+   GLUT
+   GLEW
+*/
+
+// DEFINES
+#define gradWindow
+#define gradSearch
+#define GD_REPEAT
+#define GD_WINDOW
+#define GD_Clear
+#define gradColorClear
+
+// VARS
+int GD_INT = 10;
+float GD_FLOAT = 10.5f;
+char GD_CHAR = 'G';
+double GD_DOUBLE = 10.4;
+string GD_STRING = "Hello Grad!";
